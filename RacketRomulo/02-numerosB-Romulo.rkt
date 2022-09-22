7525dfd6c06b451acf9f8da1517690b6ebcdb6bc
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-09-21 21:01:26 -0300

    update

---
+++ RacketRomulo/02-numerosB-Romulo.rkt
@@ -0,0 +1,20 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname 02-numerosB-Romulo) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+;; Introdução à Linguagem Racket (Scheme)
+;; Prof. Ausberto S. Castro Vera       (ascv@uenf.br)
+;; UENF-CCT-LCMAT - Curso de Ciencia da Computacao
+;; 2022
+;; Aluno: Rômulo Souza Fernandes
+;;
+;;;;;;;;;;;;;;;;   Escolha a linguagem "Determine language from source"
+;;
+; ------------------------------------------------
+(display "UENF-CCT-LCMAT-CC, 2022")
+(newline)
+(display "Paradigmas de Linguagens de Programação (Prof. Ausberto Castro)")
+(newline)
+(display "Aluno: Rômulo Souza Fernandes ")
+(newline)
+(newline)
+(display (/ (sqrt (+ (+ (+ 49 5)(sin (- 18 7)))(cos (+ 20 2)))) (*(+ 5 3))16 ))
