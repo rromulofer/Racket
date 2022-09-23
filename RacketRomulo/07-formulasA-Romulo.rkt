97c0ce61cca0619405f0a52f534f2ed3d071afdd
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-09-23 01:01:46 -0300

    update

---
+++ RacketRomulo/07-formulasA-Romulo.rkt
@@ -0,0 +1,20 @@
+;; Introdução à Linguagem Scheme-Racket
+;; Prof. Ausberto S. Castro Vera       (ascv@uenf.br)
+;; UENF-CCT-LCMAT - Curso de Ciencia da Computacao
+;; Setembro - 2022
+;; Aluno: Rômulo Souza Fernandes
+;;
+#lang racket      ;; define a linguagem default
+; ------------------------------------------------
+(display "  UENF-CCT-LCMAT-CC, 2022")
+(newline)
+(display "  Paradigmas de Linguagens de Programação (Prof. Ausberto Castro)")
+(newline)
+(display "  Aluno:  Rômulo Souza Fernandes ")
+(newline)
+
+(define (fatorial n)
+    (if (= n 0)
+        1
+        (* n (fatorial (- n 1)))))
+(fatorial 6)
