94354cac0793fecc10f1f36e264336b5e7ece378
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-09-23 21:55:52 -0300

    update

--- RacketRomulo/ex-slide-3a7.rkt
+++ RacketRomulo/ex-slide-3a7.rkt
@@ -16,20 +16,31 @@
 (newline)
 (display "Lista 1 = ")
 lista1
+(newline)
 
 (display "Segundo elemento da lista: ")
 (car(cdr lista1))
+(newline)
 
 (display "Antepenúltimo elemento da lista: ")
 (car(cdr(cdr lista1)))
+(newline)
+
+(display "O elemento 8 pertence a lista 1? ")
+(member 8 (list 1 2 3 4 5))
+(display "Obs: #f = não pertence e #t = pertence ")
+(newline)
+(newline)
 
 (define lista2 (list 'a 'b 'c 'd 'e))
 (display "Lista 2 = ")
 lista2
+(newline)
 
 (define lista2a (cons 'a (cons 'b (cons 'c (cons 'd (cons 'e '(3)))))))
 (display "Lista 2 com terceiro elemento no final da lista: ")
 lista2a
+(newline)
 
 (define lista2b (cons '3 '(a b c d e)))
 (display "Lista 2 com terceiro elemento no inicio da lista: ")
